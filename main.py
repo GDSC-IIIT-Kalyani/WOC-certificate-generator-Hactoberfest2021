acc4cf8df61945a11d1a832fa47a67e8e4f0058a
Ayushman Bhatt <theperfectionist0922@gmail.com>
2021-10-12 04:31:23 +0530

    Update main.py
    added code functionality for emailing certificates

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-from flask import Flask,render_template,redirect,request
-import requests 
+from flask import Flask,render_template,request
+
 from PIL import Image as a1
 from PIL import ImageDraw as a2
 from PIL import ImageFont as a3
-import pandas as pan
+
 
 import imghdr
 import smtplib
 from email.message import EmailMessage
 
 app = Flask(__name__)
-
+codes = ["A","B","C","D","E","F","G","H","I","J","K","L"]
+winners =12
 @app.route("/")
 def Index():
     return render_template("index.html")
@@ -19,15 +20,15 @@ def Index():
 def form():
 
     # Enter your email and password  from which you want to sent the image
-    Your_email = "dummy"
-    Your_Password = "dummy"
+ Your_email = "dummy e-mail"
+ Your_Password = "dummy password"
 
-
-    email = request.form.get("email")
-    name = request.form.get("name")
-  
-    font = a3.truetype('arial.ttf', 65)
-    
+ email = request.form.get("email")
+ name = request.form.get("name")
+ code = request.form.get("code")
+ font = a3.truetype('arial.ttf', 65)
+ for i in range(0,winners):
+   if(code == codes[i]):
     ay1 = a1.open('sample_cert.jpg')
     draw = a2.Draw(ay1)
     draw.text(xy=(401, 234), text='{}'.format(name), fill=(0, 0, 0), font=font)
@@ -54,16 +55,7 @@ def form():
     server.send_message(newMessage)
    
     return render_template("result.html")
-
-
+ return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-# ayush = pan.read_csv('name_of_cert_owners.csv')
-# font = a3.truetype('arial.ttf', 65)
-# for index, i in ayush.iterrows():
-#     ay1 = a1.open('sample_cert.jpg')
-#     draw = a2.Draw(ay1)
-#     draw.text(xy=(401, 234), text='{}'.format(i['name']), fill=(0, 0, 0), font=font)
-#     ay1.save('cert_save_folder/{}.jpg'.format(i['name']))
